0761441a1ef53bee533eca4f7e1a4a1bffcc235a
keshavbansal20 <bansal.keshav.mathura@gmail.com>
2022-01-17 01:18:01 +0530

    queenPermutation

--- Recursion_And_BAcktracking/levelup/RecursionTrees.java
+++ Recursion_And_BAcktracking/levelup/RecursionTrees.java
@@ -137,7 +137,41 @@ public class RecursionTrees {
         count+=permutationWithSingleCoin_subSeq(arr, tar, idx+1, ans);
         return count;
     }
-    public static void main(String[] args){
+
+    // 1D Queen_Set================================
+    public static int queenCombination(int tbox , int tqns , int qpsf , int bn , String ans){
+        if(tqns==qpsf){
+            System.out.println(ans);
+            return 1;
+        }
+        int count = 0;
+        for(int i =  bn; i < tbox ;i++){
+            count+=queenCombination(tbox, tqns, qpsf+1, i+1, ans+"b"+i+"q"+qpsf+" ");
+        }
+        return count;
+    }
+
+    public static int queenPermutation(boolean[] tbox , int tqns , int qpsf , int bn , String ans){
+        
+        if(qpsf==tqns){
+            System.out.println(ans);
+            return 1;
+        }
+        int count=0;
+        for(int i = bn; i < tbox.length ; i++){
+            if(!tbox[i]){
+
+                tbox[i] = true;
+                count+=queenPermutation(tbox, tqns, qpsf+1, 0 , ans + "b"+i+"q"+ qpsf+" ");
+                tbox[i] = false;
+            }
+
+        }
+        return count;
+
+    }
+    public static void coinChange(){
+
         int[] arr = { 2, 3 , 5 , 7};
         int tar = 10;
         // System.out.println(permutationWithInfi(arr , tar  , ""));
@@ -147,7 +181,18 @@ public class RecursionTrees {
         
         // System.out.println(permutationWithInfi_subSeq(arr, tar, 0, ""));
         System.out.println(combinationWithInfi_subSeq(arr , tar , 0 , ""));
+    }
 
+    public static void queenSet(){
+        boolean[] boxes = new boolean[6];
+        // System.out.println(queenCombination(6 , 4 , 0 , 0 , ""));
+        System.out.println(queenPermutation(boxes, 4 , 0 , 0 , ""));
+
+
+
+    }
+    public static void main(String[] args){
+        queenSet();
 
 
     }
